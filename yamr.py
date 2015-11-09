bbb6c1af2a46ecd0037a0fcfab6de7ef4ddfbb3d
Marco Unternährer <marco.unter@gmail.com>
2015-11-09 10:15:13 +0100

    Add index template, loading boostrap and jquery

--- yamr.py
+++ yamr.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('helloworld.html')
 
 
 if __name__ == '__main__':
