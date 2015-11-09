c16c85be4842dfb9a9138b1c27e366f74e836462
Marco Unternährer <marco.unter@gmail.com>
2015-11-09 15:03:35 +0100

    change file structure

--- yamr_webapp/yamr.py
+++ yamr_webapp/yamr.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify, render_template, request
+from yamr_ml.recommender import RecommendationEngine
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    app.logger.debug('A value for debugging')
     return render_template('helloworld.html')
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
