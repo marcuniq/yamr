f2ae24bf3b47628d404dc5e105eaa00fd767429f
Marco Unternährer <marco.unter@gmail.com>
2015-11-23 19:45:39 +0100

    extending movie infos via a wrapper of"the movie db" api

--- yamr/app.py
+++ yamr/app.py
@@ -1,12 +1,27 @@
 from flask import Flask, jsonify, render_template, request
-from json import dumps
-
+import tmdbsimple as tmdb
 app = Flask(__name__)
 
 from yamr.database import Database
-
 dataset_path = 'datasets/ml-latest'
 db = Database(dataset_path)
+
+tmdb.API_KEY = tmdb._get_env_key('TMDB_API_KEY')
+
+
+def extend_movie_info(movie):
+    tmdb_m = tmdb.Movies(movie['tmdbId'])
+
+    poster_url = 'http://image.tmdb.org/t/p/w185' + str(tmdb_m.images()['posters'][0]['file_path'])
+    movie['poster'] = poster_url
+
+    youtube_videos = filter(lambda r: r['site'] == 'YouTube', tmdb_m.videos()['results'])
+    if len(youtube_videos) != 0:
+        youtube_key = youtube_videos[0]['key']
+        youtube_url = 'https://www.youtube.com/embed/' + str(youtube_key)
+        movie['trailer'] = youtube_url
+
+    return movie
 
 
 @app.route('/')
@@ -17,6 +32,7 @@ def index():
 @app.route('/api/movies/<int:movieId>')
 def movie_detail(movieId):
     movie = db.find_movie_by_id(movieId)
+    movie = extend_movie_info(movie)
     return jsonify(movie)
 
 
